c09dc61bb6a2cf5d1dafedd8a3fc09ae33b824cc
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-13 11:52:24 +0530

    Completed 2 Questions

---
+++ AbdulRazak/13_12_2023/group.mjs
@@ -0,0 +1,72 @@
+ // 1 - group according to value 
+ // top 5 rank according to sales 
+ class DataSet {
+    header = [];
+    values = [];
+    constructor(list){
+    this.header = list[0];
+    this.values = list.slice(1);    
+    }
+    groupBy(min1 , min2 , min3 , col ){
+        let index = this.header.indexOf(col);
+        if(index==-1){
+            throw new Error("cant find the column name")
+        }
+        let arr =  this.values;
+        this.header.push("Group");
+        for(let i = 0  ; i < arr.length ; i++){
+            let sale =  parseFloat(arr[i][index]);
+            if(sale >= min1 && sale<min2){
+                arr[i].push("poor");
+            }else if(sale >= min2 && sale<min3){
+                arr[i].push("neutral");
+            }else arr[i].push("good");
+        }
+        this.values = arr;
+        console.log(arr)
+    }
+    filterByGroup(col ,grp){
+        let arr =   this.values;
+        let index = this.header.indexOf(col);
+        if(index==-1){
+            throw new Error("cant find the column name")
+        }
+        let newArr = arr.filter(ar => ar[index]==grp);
+        newArr.unshift(this.header);
+        return newArr;
+    }
+    rankBy(col,n){
+        let index = this.header.indexOf(col);
+        this.header.push("Rank")
+        if(index==-1){
+            throw new Error("cant find the column name")
+        }
+         let arr =  this.values;
+         arr.sort((a,b)=>b[index]-a[index])
+         arr[0].push(1);
+         let rank = 1 ;
+         for(let i = 1 ; i < arr.length ; i++){
+            if(arr[i-1][index] == arr[i][index]){
+                arr[i].push(rank);
+            }else{
+                rank++;
+                arr[i].push(rank);
+            }
+
+         }
+         this.values =  arr
+         let newArr = []
+         for(let i  = 0 ; i < n ; i++){
+            newArr.push(arr[i]);
+         }
+         newArr.unshift(this.header)
+         return newArr;
+    }
+
+    
+
+
+
+
+ }
+ export default DataSet;
---
+++ AbdulRazak/13_12_2023/groupTest.mjs
@@ -0,0 +1,33 @@
+import DataSet from "./group.mjs";
+let salesData = [
+    ["Product", "Sales"],
+    ["Milkybar", "200"],
+    ["Silk", "400"],
+    ["Dairy Milk", "700"],
+    ["Eclaires", "800"],
+    ["Nesley", "1000"],
+    ["Kitkat", "100"],
+    ["Five Star", "200"],
+    ["Snikkers", "500"],
+    ["Munch", "300"],
+    ["Perk", "400"],
+    ["Center Fresh", "600"],
+    ["Mint", "800"],
+    ["Polo", "200"],
+    ["Tictac", "400"],
+    ["Gems", "100"],
+    ["Kinder Joy", "900"],
+    ["Marbels", "100"],
+    ["Melody", "500"],
+    ["Mentos", "300"],
+    ["Coffee Bite", "200"],
+    ["Luvit", "800"],
+    ["Kopiko", "1000"],
+    ["Kacha Mango", "800"],
+    ["Lollipop", "1000"],
+  ];
+ 
+  let grpData =  new DataSet(salesData);
+  grpData.groupBy(100,300,500,"Sales")
+  console.log(grpData.filterByGroup("Group","good"))
+  console.log(grpData.rankBy("Sales",5))
