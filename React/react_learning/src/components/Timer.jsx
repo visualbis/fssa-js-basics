66c42159e320ad1270fde74832d8b9cd49f803cd
Susikumar-Pitchaimuthu <susikumar.pitchaimuthu@gmail.com>
2023-12-26 15:32:43 +0530

    Timer Flag Function Added

--- React/react_learning/src/components/Timer.jsx
+++ React/react_learning/src/components/Timer.jsx
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "../css/Timer.css";
+
 const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [timestamps, setTimestamps] = useState([]);
 
   useEffect(() => {
     let timer;
 
     if (isRunning) {
       timer = setInterval(() => {
-        setSeconds((prevSeconds) => {
-          if (prevSeconds === 59) {
-            setMinutes((prevMinutes) => {
-              if (prevMinutes === 59) {
-                setHours((prevHours) => prevHours + 1);
+        setMilliseconds((prevMilliseconds) => {
+          if (prevMilliseconds === 99) {
+            setSeconds((prevSeconds) => {
+              if (prevSeconds === 59) {
+                setMinutes((prevMinutes) => prevMinutes + 1);
                 return 0;
               } else {
-                return prevMinutes + 1;
+                return prevSeconds + 1;
               }
             });
-            setSeconds(0);
+            return 0;
           } else {
-            return prevSeconds + 1;
+            return prevMilliseconds + 1;
           }
         });
-      }, 100);
+      }, 1);
     }
 
     return () => {
@@ -37,38 +39,62 @@ const Timer = () => {
   const handleRunClick = () => {
     setIsRunning(true);
   };
+
   const handleStopClick = () => {
     setIsRunning(false);
   };
 
   const handleResetClick = () => {
     setIsRunning(false);
+    setMilliseconds(0);
     setSeconds(0);
     setMinutes(0);
-    setHours(0);
+    setTimestamps([]);
+  };
+
+  const handleFlagClick = () => {
+    setTimestamps([
+      ...timestamps,
+      `${formatTimeUnit(minutes)}:${formatTimeUnit(seconds)}.${formatTimeUnit(
+        milliseconds
+      )}`,
+    ]);
+  };
+
+  const formatTimeUnit = (unit) => {
+    return unit < 10 ? `0${unit}` : unit;
   };
 
   return (
     <div className="timer-div-container">
       <div className="timer-inside-div">
         <div className="timer-head-div">
-          <h1 className="timer-head">Timer</h1>
+          <h1 className="timer-head">Stopwatch</h1>
         </div>
         <div className="timer-showing-div">
           <p className="timer-para">
-            Timer: {hours + " : " + minutes + " :" + seconds} seconds
+            Time: {formatTimeUnit(minutes)}:{formatTimeUnit(seconds)}.
+            {formatTimeUnit(milliseconds)}
           </p>
+          <div className="timestamps">
+            {timestamps.map((timestamp, index) => (
+              <p key={index}>{`Timestamp ${index + 1}: ${timestamp}`}</p>
+            ))}
+          </div>
         </div>
         <div className="timer-controller-button-div">
           <div className="timer-controller-button-inside-div">
             <button className="time-run-button" onClick={handleRunClick}>
-              <i class="bi bi-play-circle"></i>Run
+              <i className="bi bi-play-circle"></i>Run
             </button>
             <button className="time-stop-button" onClick={handleStopClick}>
-              <i class="bi bi-pause-circle"></i>Stop
+              <i className="bi bi-pause-circle"></i>Stop
             </button>
             <button className="reset-time-button" onClick={handleResetClick}>
-              <i class="bi bi-vinyl"></i> Reset
+              <i className="bi bi-vinyl"></i>Reset
+            </button>
+            <button className="add-time-stamp-button" onClick={handleFlagClick}>
+              <i className="bi bi-flag"></i>Flag
             </button>
           </div>
         </div>
