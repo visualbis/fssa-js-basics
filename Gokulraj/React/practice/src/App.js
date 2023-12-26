34dd4f3e6ef45a37edd7f0d375bc467d2bdd97ef
gokulraj <gokulrajmurugan040506@gmail.com>
2023-12-26 17:48:21 +0530

    Tic Tac Toe game completed

--- Gokulraj/React/practice/src/App.js
+++ Gokulraj/React/practice/src/App.js
@@ -1,80 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
+import Today from "./components/TodayList";
+import TodoList from './components/TodoList';
+// import Popup from './components/Popup'
 
 function App() {
-
-  const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState([]);
-  const [editId, setEditId] = useState(0);
-
-
-
-  function addTask(e) {
-    e.preventDefault();
-
-    if (editId) {
-      const editTask = tasks.find((oneTask) => oneTask.id === editId);
-
-      const update = tasks.map((row) => row.id === editTask.id ? row = { id: row.id, "taskName": newTask } : { id: row.id, "taskName": row.taskName })
-
-      setTasks(update)
-      setNewTask("");
-      setEditId(0);
-      return;
-    }
-
-
-    if (newTask.trim() !== "") {
-      setTasks([{ id: tasks.length + 1, "taskName": newTask }, ...tasks]);
-      setNewTask("");
-    }
-  }
-
-  function deleteTask(id) {
-    const del = tasks.filter((oneTask) => oneTask.id !== id);
-    setTasks([...del]);
-    setNewTask("");
-    return;
-  }
-
-  function updateTask(id) {
-    const update = tasks.find((taskid) => taskid.id === id);
-    setNewTask(update.taskName);
-    setEditId(id);
-
-
-
-  }
-
   return (
-    <div className="all">
-      <div className='head'>
-
-        <h3 className="title">Add a New Task!</h3>
-
-        <form onSubmit={addTask}>
-          <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
-          <button type='submit'> {editId ? "Edit" : "Add"} </button>
-        </form>
-
-
-      </div>
-      
-      <div className='todo-list'>
-        <ul>
-          {tasks.map((t) => (
-            <li className='single-todo' key={t.id}>
-              <span key={t.id}>
-                {t.taskName}
-              </span>
-              <button onClick={() => updateTask(t.id)}>Edit</button>
-              <button onClick={() => deleteTask(t.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+    <>    <div className="app-container" >
+      <div className="left-side">
+        <TodoForm />
+        <Today />
       </div>
 
+      <div className="right-side">
+        <TodoList />
+      </div>
     </div>
+    </>
+
   )
 }
 
--- Gokulraj/React/practice/src/index.js
+++ Gokulraj/React/practice/src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import TodoForm from './components/TodoForm';
 import reportWebVitals from './reportWebVitals';
-import Today from "./components/TodayList";
-import TodoList from './components/TodoList';
-import Popup from './components/Popup'
+import './index.css';
+// import App from './App';
+import TicTacToe from './components/TicTacToe';
+// import PomodoroTimer from './components/Stopwatch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="app-container" >
-      <div className="left-side">
-        <TodoForm />
-        <Today />
-      </div>
-      
-      <div className="right-side">
-        <TodoList />
-      </div>
-    </div>
+    {/* <App /> */}
+    {/* <PomodoroTimer /> */}
+    <TicTacToe />
   </React.StrictMode>
 );
 
