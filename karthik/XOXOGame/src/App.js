93cb387143776be4e903561343f47477928e7303
karthik p <108673648+karthik0410@users.noreply.github.com>
2023-12-28 09:18:43 +0530

    TicTacToe-Updated

---
+++ karthik/XOXOGame/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTacToe from "./component/Assets/TicTacToe/TicTacToe.jsx";
+
+function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
